db8198d5cf845631195d4a7a579cd06e4855138b
silkysuede <silkysuede@users.noreply.github.com>
2021-04-07 23:41:36 -0500

    Add project files.

---
+++ AppFromScratch/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AppFromScratch.Models;
+namespace AppFromScratch.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.CurrentDate = DateTime.Now;
+            return View();
+        }
+        [HttpGet]
+        public ActionResult SignupForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignupForm(UserResponses userResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("ThankYou", userResponse);
+            }
+            else { return View(); }
+            
+        }
+    }
+}
---
+++ AppFromScratch/Models/UserResponses.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppFromScratch.Models
+{
+    public class UserResponses
+    {
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number.")]
+        public string Phone { get; set; }
+        public bool Affiliation { get; set; }
+        [Required(ErrorMessage = "Please enter your Age.")]
+        public string Age { get; set; }
+
+    }
+}
